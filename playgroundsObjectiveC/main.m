d39c7ba93c72fd67c8cb4a6e519714893d1129cf
Igor Chernyshov <ichernyshov@bk.ru>
2019-04-25 20:27:03 +0300

    Initial Commit

---
+++ playgroundsObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  playgroundsObjectiveC
+//
+//  Created by Igor Chernyshov on 25/04/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
