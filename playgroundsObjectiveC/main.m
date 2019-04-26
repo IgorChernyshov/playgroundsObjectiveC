ba4deaf6a89fca0b81e7aa8402279799ef918f50
Igor Chernyshov <ichernyshov@bk.ru>
2019-04-26 07:45:59 +0300

    Added methods to play with variables

--- playgroundsObjectiveC/main.m
+++ playgroundsObjectiveC/main.m
@@ -8,10 +8,57 @@
 
 #import <Foundation/Foundation.h>
 
+#define MAGICALNUMBER 10
+
+void declareVariables (void)
+{
+  int intValue = 1;
+  char *charValue = "kindaChar";
+  bool boolValue = true;
+  float floatValue = 6.5;
+  double doubleValue = 10.8;
+  
+  BOOL boolObjectiveC = YES;
+  NSInteger integer = 1;
+  CGFloat cgFloat = 14.2;
+  NSNumber *number = @128;
+  NSString *string = @"NSString here";
+  
+  NSLog(@"%d", intValue);
+  NSLog(@"%s", charValue);
+  NSLog(@"%d", boolValue);
+  NSLog(@"%f", floatValue);
+  NSLog(@"%f", doubleValue);
+  NSLog(@"%d", boolObjectiveC);
+  NSLog(@"%ld", (long)integer);
+  NSLog(@"%f", cgFloat);
+  NSLog(@"%@", number);
+  NSLog(@"%@", string);
+}
+
+void declareConst (void)
+{
+  NSString * const immutableString = @"This is an immutable string";
+  // Can't do: immutableString = @"Changed string";
+  NSLog(@"%@", immutableString);
+  
+  NSInteger integer = MAGICALNUMBER;
+  NSLog(@"%ld", (long)integer);
+}
+
+void castFloatToInt (void)
+{
+  float someFloat = 20.999;
+  int intFromFloat = (int)someFloat;
+  
+  NSLog(@"Int from %f is %d", someFloat, intFromFloat);
+}
+
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+    //declareVariables();
+    //declareConst();
+    castFloatToInt();
   }
   return 0;
 }
