9e0eaf684d6a722009d3be1768179fa04d3b76a3
Igor Chernyshov <ichernyshov@bk.ru>
2019-04-27 13:48:28 +0300

    Added switch operator

---
+++ playgroundsObjectiveC/conditionalOperators.m
@@ -0,0 +1,38 @@
+//
+//  conditionalOperators.m
+//  playgroundsObjectiveC
+//
+//  Created by Igor Chernyshov on 27/04/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+float calculate(NSInteger number1, char operator, NSInteger number2)
+{
+  switch (operator) {
+    case '+':
+      return number1 + number2;
+      break;
+    case '-':
+      return number1 - number2;
+      break;
+    case '*':
+      return number1 * number2;
+      break;
+    case '/':
+      if (number2 == 0) {
+        NSLog(@"You cannot divide by zero");
+        return 0.0;
+      }
+      return (float)number1 / number2;
+      break;
+    case '%':
+      return number1 % number2;
+      break;
+    default:
+      NSLog(@"Unknown / unsupported operator");
+      return 0.0;
+      break;
+  }
+}
--- playgroundsObjectiveC/main.m
+++ playgroundsObjectiveC/main.m
@@ -8,11 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+float calculate(NSInteger number1, char operator, NSInteger number2);
+
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
     //declareVariables();
     //declareConst();
     //castFloatToInt();
+    //float sum = calculate(10, '+', 15);
+    //NSLog(@"%f", sum);
+    float divide = calculate(12345, '/', 23);
+    NSLog(@"%f", divide);
   }
   return 0;
 }
